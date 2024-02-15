d49dbab32ada2528c1e405eb413ce97c098ae1a6
Firas Aleem <faa135@student.bham.ac.uk>
2024-02-15 09:24:25 +0000

    Added script for building wasm and copying packages

---
+++ build-wasm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# The script builds and moves the warm folders to the right places
+# Run after changes to the Rust code
+
+# Determine the base directory relative to the script location
+BASE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Navigate to the RustCode directory relative to the script's location
+cd "$BASE_DIR/RustCode" || exit
+
+# Build for web target
+wasm-pack build --target web --out-dir web_wasm
+
+# Replace the existing web_wasm folder with the new one
+rm -rf "$BASE_DIR/Website/public/web_wasm"
+cp -R web_wasm "$BASE_DIR/Website/public/web_wasm"
+
+# Build for nodejs target
+wasm-pack build --target nodejs --out-dir node_wasm
+
+# Replace the existing node_wasm folder with the new one
+rm -rf "$BASE_DIR/Website/node_wasm"
+cp -R node_wasm "$BASE_DIR/Website/node_wasm"
